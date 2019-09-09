1aee444dd261d6a680ca897d9cc11dde347c64da
wubowen <wubowen1992@outlook.com>
2019-09-09 22:55:06 +0800

    feat:First commit

---
+++ mainwindow.cpp
@@ -0,0 +1,13 @@
+#include "mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+{
+    setFixedSize(800, 640);
+    setWindowTitle("邻家小厨账目管理");
+}
+
+MainWindow::~MainWindow()
+{
+
+}
